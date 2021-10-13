4f424a1afd469e1afe9e6af8f3323cd8a110e4b8
RandyPan <randy4mac@gmail.com>
2021-10-14 00:32:40 +0800

    增加專案結構

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,9 +1,17 @@
 package repository
 
-var users = []User{
-	{ID: 1, Name: "张三"},
-	{ID: 2, Name: "李四"},
-	{ID: 3, Name: "王五"},
+var Users []User
+
+func GetAllUsers() (users []User) {
+	if Users == nil {
+		Users = make([]User, 100)
+	}
+	return Users
+}
+
+func AddNewUser(user User) (users []User) {
+	Users = append(Users, user)
+	return Users
 }
 
 type User struct {
--- main.go
+++ main.go
@@ -1,69 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"strconv"
 
+	"github.com/RandyPanGit/bin-zang-go/internal/delivery"
 	"github.com/gorilla/mux"
 )
 
-var users = []User{
-	{ID: 1, Name: "张三"},
-	{ID: 2, Name: "李四"},
-	{ID: 3, Name: "王五"},
-}
-
-type User struct {
-	ID   int
-	Name string
-}
-
 func main() {
 	mux := mux.NewRouter()
-	mux.HandleFunc("/user/info/{id}", getUser).Methods(http.MethodGet)
-	// http.HandleFunc("/user", getUser)
-	// http.HandleFunc("/user/{id}", handleUsers)
+	delivery.BuildRoute(mux)
 	http.Handle("/", mux)
-	http.ListenAndServe(":8082", nil)
-}
-
-func handleUsers(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		users, err := json.Marshal(users)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, "{\"message\": \""+err.Error()+"\"}")
-		} else {
-			w.WriteHeader(http.StatusOK)
-			w.Write(users)
-		}
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "{\"message\": \"not found\"}")
-	}
-}
-
-func getUser(w http.ResponseWriter, r *http.Request) {
-	method := r.Method
-	vars := mux.Vars(r)
-	id := vars["id"]
-	idInt, _ := strconv.Atoi(id)
-	if method == "GET" {
-		var user User
-		for i := 0; i < len(users); i++ {
-			userId := users[i].ID
-			if userId == idInt {
-				user = users[i]
-			}
-		}
-		userData, _ := json.Marshal(user)
-		w.WriteHeader(http.StatusOK)
-		w.Write(userData)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "{\"message\": \"not found\"}")
-	}
+	http.ListenAndServe(":8080", nil)
 }
