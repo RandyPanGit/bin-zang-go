f49e78e3de438aaf50ff7b9d75a7b8cc6c552725
randypan <randy.pan@tpisoftware.com>
2021-11-08 17:28:23 +0800

    add heroku port setting

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/RandyPanGit/bin-zang-go/internal/delivery"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	var port string
 	mux := mux.NewRouter()
 	delivery.BuildRoute(mux)
+	port, _ = os.LookupEnv("PORT")
 	http.Handle("/", mux)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(port, nil)
 }
